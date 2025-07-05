41d84ab76b7dd8c906b0adc31759576c96da771a
xamidillo8 <xamidillofoziljonov2@gmail.com>
2025-07-05 15:40:20 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores reiciendis quasi nulla iste facere deserunt accusamus fugiat minus deleniti velit, ea rem a debitis illo eveniet saepe temporibus odit maiores?
+    </div>
+  )
+}
+
+export default App
